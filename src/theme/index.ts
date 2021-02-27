6b81f9f84c293996c0d0c835b9babe2eb58b5ac2
webcore-it <info@webcore-it.com>
2021-02-27 15:38:42 +0100

    Removes preset for a more simple example.

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,5 +1,4 @@
-import {merge, Theme} from "theme-ui";
-import {base as preset} from "@theme-ui/presets";
+import {Theme} from "theme-ui";
 
 const VARIANTS = {
     primary: {
@@ -9,21 +8,52 @@ const VARIANTS = {
     }
 };
 
-export const CustomTheme: Theme = merge(
-    preset as Theme,
+export const CustomTheme: Theme =
     {
+        space: [
+            0,
+            4,
+            8,
+            16,
+            32,
+            64,
+            128,
+            256,
+            512
+        ],
+        fonts: {
+            body: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+            heading: 'inherit',
+            monospace: 'Menlo, monospace'
+        },
+        fontSizes: [
+            12,
+            14,
+            16,
+            20,
+            24,
+            32,
+            48,
+            64,
+            96
+        ],
+        fontWeights: {
+            body: 400,
+            heading: 700,
+            bold: 700
+        },
+        lineHeights: {
+            body: 1.5,
+            heading: 1.125
+        },
         colors: {
+            muted: '#f6f6f6',
             dark: "#040404",
             bright: "#f0f0f0",
             primary: "#007777",
             secondary: "#770077",
             background: "bright",
             text: "dark",
-        },
-        fonts: {
-            body: 'HelveticaNeue, "Helvetica Neue", sans-serif',
-            heading: 'inherit',
-            monospace: 'monospace',
         },
         alerts: {
             ...VARIANTS
@@ -33,6 +63,90 @@ export const CustomTheme: Theme = merge(
         },
         messages: {
             ...VARIANTS
+        },
+        styles: {
+            root: {
+                fontFamily: 'body',
+                lineHeight: 'body',
+                fontWeight: 'body'
+            },
+            h1: {
+                color: 'text',
+                fontFamily: 'heading',
+                lineHeight: 'heading',
+                fontWeight: 'heading',
+                fontSize: 5
+            },
+            h2: {
+                color: 'text',
+                fontFamily: 'heading',
+                lineHeight: 'heading',
+                fontWeight: 'heading',
+                fontSize: 4
+            },
+            h3: {
+                color: 'text',
+                fontFamily: 'heading',
+                lineHeight: 'heading',
+                fontWeight: 'heading',
+                fontSize: 3
+            },
+            h4: {
+                color: 'text',
+                fontFamily: 'heading',
+                lineHeight: 'heading',
+                fontWeight: 'heading',
+                fontSize: 2
+            },
+            h5: {
+                color: 'text',
+                fontFamily: 'heading',
+                lineHeight: 'heading',
+                fontWeight: 'heading',
+                fontSize: 1
+            },
+            h6: {
+                color: 'text',
+                fontFamily: 'heading',
+                lineHeight: 'heading',
+                fontWeight: 'heading',
+                fontSize: 0
+            },
+            p: {
+                color: 'text',
+                fontFamily: 'body',
+                fontWeight: 'body',
+                lineHeight: 'body'
+            },
+            a: {
+                color: 'primary'
+            },
+            pre: {
+                fontFamily: 'monospace',
+                overflowX: 'auto',
+                code: {
+                    color: 'inherit'
+                }
+            },
+            code: {
+                fontFamily: 'monospace',
+                fontSize: 'inherit'
+            },
+            table: {
+                width: '100%',
+                borderCollapse: 'separate',
+                borderSpacing: 0
+            },
+            th: {
+                textAlign: 'left',
+                borderBottomStyle: 'solid'
+            },
+            td: {
+                textAlign: 'left',
+                borderBottomStyle: 'solid'
+            },
+            img: {
+                maxWidth: '100%'
+            }
         }
-    } as Partial<Theme>
-);
+    };
