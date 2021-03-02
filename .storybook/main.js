62465d1efcc2dbe19ffe9c167413dc9c4d167ee5
webcore-it <info@webcore-it.com>
2021-03-02 09:32:00 +0100

    Adds aliases to Storybooks webpack config for emotion 10 packages.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,28 @@
+const path = require("path");
+
+// Location of root node_modules
+const modulesDir = path.join(__dirname, '../node_modules');
+
+// As long Storybook depends on emotion 10.
+const emotion10Aliases = (config) => ({
+  ...config,
+  resolve: {
+    ...config.resolve,
+    alias: {
+      ...config.resolve.alias,
+      '@emotion/core': path.join(modulesDir, '@emotion/react'),
+      '@emotion/styled': path.join(modulesDir, '@emotion/styled'),
+      '@emotion/styled-base': path.join(modulesDir, '@emotion/styled'),
+      'emotion-theming': path.join(modulesDir, '@emotion/react'),
+    },
+  },
+});
+
+
 module.exports = {
   stories: [
     '../src/**/*.stories.@(ts|tsx)',
   ],
   addons: [],
+  webpackFinal: emotion10Aliases,
 };
